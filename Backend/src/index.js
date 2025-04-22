8689545ffa102431cad06ec087c9d757a6e2f2cc
Akash <Akash.patel.ap88020@gmail.com>
2025-04-22 16:55:39 +0530

    initial setup

---
+++ Backend/src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+import authRoutes from './routes/auth.route.js';
+
+app.use('/api/auth/', authRoutes);
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
